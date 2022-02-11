aa4796389d881d0ffdcfe8d7a617b5c1bece35d6
Mariano Tugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2022-02-11 00:15:22 -0300

    Escenarios de Bienvenida implementados sobre la API

--- src/apiAcceptanceTest/java/ar/com/grupoesfera/repartir/api/steps/inicio/BienvenidaSteps.java
+++ src/apiAcceptanceTest/java/ar/com/grupoesfera/repartir/api/steps/inicio/BienvenidaSteps.java
@@ -2,50 +2,59 @@ package ar.com.grupoesfera.repartir.api.steps.inicio;
 
 import ar.com.grupoesfera.repartir.api.steps.CucumberSteps;
 import io.cucumber.java.Before;
-import io.cucumber.java.PendingException;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.cucumber.java.es.Dado;
+import io.restassured.response.ValidatableResponse;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class BienvenidaSteps extends CucumberSteps {
+
+    private String usuario;
+    private ValidatableResponse response;
 
     @Then("el usuario accede a la aplicación")
     public void elUsuarioAccedeALaAplicacion() {
 
-        throw new PendingException();
+
     }
 
     @Then("se muestra el mensaje de bienvenida")
     public void seMuestraElMensajeDeBienvenida() {
 
-        throw new PendingException();
+
     }
 
     @Given("que el usuario accedió a la aplicación")
     public void queElUsuarioAccedioALaAplicacion() {
 
-        throw new PendingException();
+
     }
 
     @When("decidió iniciar")
     public void decidioIniciar() {
 
-        throw new PendingException();
+        usuario = "catalina";
+        response = when()
+                .get(url("/api/usuarios/{usuario}"), usuario)
+                .then();
+
     }
 
     @Then("puede empezar a usarla")
     public void puedeEmpezarAUsarla() {
 
-        throw new PendingException();
+        response.body("id", equalTo(usuario))
+                .body("correo", equalTo(usuario + "@repartir.com"));
     }
 
     @Dado("que el usuario inició Repartir")
     public void elUsuarioInicioRepartir() {
 
         baseDeDatos.estaVacia();
-
-        throw new PendingException();
     }
 
     @Before
