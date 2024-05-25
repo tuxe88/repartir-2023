b96c0ee8d691da77823857979c4384c76f6ea137
tuxe88 <v.tourrilhes@gmail.com>
2024-05-25 16:05:53 -0300

    Implementación de los tests de aceptación y de la funcionalidad

---
+++ src/main/java/ar/com/grupoesfera/repartir/exceptions/GruposNombresDuplicadosException.java
@@ -0,0 +1,7 @@
+package ar.com.grupoesfera.repartir.exceptions;
+
+public class GruposNombresDuplicadosException extends RuntimeException {
+    public GruposNombresDuplicadosException() {
+        super("dsaasddsasadasd");
+    }
+}
